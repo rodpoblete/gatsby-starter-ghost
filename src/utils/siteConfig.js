cc95bfb1fe58bb6f892ba7e6fb8ecc2b4b485921
Rodrigo Poblete <rorro.poblete@gmail.com>
2020-08-26 00:26:07 -0400

    Update siteConfig.js
    Update siteTitleMeta & shortTitle variables

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
     siteUrl: `https://rodpoblete.com`,
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `rodPoblete.com`, // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Home`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Hola mundo`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `rodPoblete`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `rodpoblete.com`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
